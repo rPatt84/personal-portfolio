f9ace25df6897033e5b50627b0a7708dfa796d73
rob <rpcoding28@googlemail.com>
2022-07-28 03:45:35 +0100

    Finished design, looks and functionality

---
+++ script/main.js
@@ -0,0 +1,22 @@
+//add all project h3 elements to an array
+const projectArticlesH3Array = document.querySelectorAll('.projects-article-h3');
+
+// add event listeners to all project h3 elements
+projectArticlesH3Array.forEach(cur => {
+    cur.addEventListener('click', (event) => {
+        //display hidden element upon project text click
+        const childNodes = event.target.parentNode.children;
+        childNodes[1].classList.toggle('collapse');
+        
+        const iElement = event.target.children[0];
+        console.log(iElement)
+        if(iElement.style.transform === '') {
+            //rotate project i element upon click
+            iElement.style.transform = 'rotate(0deg)'
+        } else {
+            //rotate element back to normal
+            iElement.style.transform = '';
+            iElement.style.transition = 'transform .75s'
+        }
+    })
+});
